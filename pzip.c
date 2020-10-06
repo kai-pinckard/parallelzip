164d4c862f5210f14cd26e671e086cd99d6d8672
kai <kai.pinckard@gmail.com>
2020-10-06 13:31:20 -0700

    design partition function

--- pzip.c
+++ pzip.c
@@ -50,7 +50,41 @@ void zip_files()
 }
 
 /*
+This helper function for partition work returns the index of the second type of character occuring in
+a file for example in the file aaaabccc it would return the index of b.
+
+desired_split_index refers to the position of the file which we desire the split to occur in
+that is contained in the list of files contained in the file_list struct. 
+returns a pointer to the last character that is to be included in the current split.
+
+
+
+
+a much simpler way to split is to instead split whereever i would like to based on fairness and then when the threads are writing to stdout they can check the last character that
+was written and if it matches the current character it can add that count to its current count and then write the data to standard out. alternatively there could be a communication variable to 
+hold the last value in the current threads chunk in this way standard out does not need to be modified. 
+*/
+char* get_split_index(File_List_t file_list, int desired_split_index)
+{
+    char* split_file = file_list.list[desired_split_index];
+    long int size = file_list.list[desired_split_index].size;
+
+    int first_char = split_file[0];
+    
+    for(long int i = 1; i < size; i++)
+    {
+        if (split_file[i] != first_char)
+        {
+            return i;
+        }
+    }
+
+    // if there are more files we must continue searching for a good split.
+}
+/*
     This function should only be called by the main thread to determine the split points for all the other threads. 
+
+    IMPORTANT NOTE even if only one character is repeating in multiple consecutive files that character must still be summed correctly without repeated counts.
 */
 struct Part_t* partition_work(char** files_to_partition)
 {
@@ -63,13 +97,25 @@ void print_file_list(File_List_t file_list)
     for(int i = 0; i < file_list.length; i++)
     {
         printf("filename: %s\n", file_list.list[i].name);
+        //printf("file contents:\n%s\n", file_list.list[i].contents);
         printf("file_size: %ld\n", file_list.list[i].size);
     }
+    printf("\nlist length: %d\n", file_list.length);
 }
 
+/* 
+File_List_t init_file_list(int length)
+{
+
+    return 
+}
+ */
 
 int main(int argc, char** argv)
 {
+    const int NUM_THREADS = 1;
+    //getenv to load the number of threads.
+
     // Since the name of the program is an argument
     int length = argc - 1;
     File_t list[length];
@@ -94,6 +140,7 @@ int main(int argc, char** argv)
             //printf("%s file size %ld\n", argv[i], statbuffer.st_size);
 
             file_list.list[i-1].name = argv[i];
+            // check for mmap fail
             file_list.list[i-1].contents = mmap(NULL, statbuffer.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
             file_list.list[i-1].size = statbuffer.st_size;
         }
